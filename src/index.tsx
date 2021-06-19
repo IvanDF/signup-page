e5c0b31ec265d779772b97b599450618d6a57e96
IvanDF <delfatti.ivan@gmail.com>
2021-06-19 18:41:11 +0200

    git init

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <span>CIAO</span>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
